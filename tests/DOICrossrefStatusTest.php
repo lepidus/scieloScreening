53d9816e931a2608b05a26dbd37dcde7ecf7141c
thiagolepidus <thiago@lepidus.com.br>
2021-06-11 10:53:06 -0400

    Fixes the DOICrossrefStatusTest class name.
    Issue: documentacao-e-tarefas/scielo#184
    
    Signed-off-by: Thiago <thiago@lepidus.com.br>

--- tests/DOICrossrefStatusTest.php
+++ tests/DOICrossrefStatusTest.php
@@ -3,7 +3,7 @@
 use PHPUnit\Framework\TestCase;
 require "DOISystemClientForTests.inc.php";
 
-final class CrossrefNonExistentDOITest extends TestCase
+final class DOICrossrefStatusTest extends TestCase
 {
     public function testIsInvalidWhenResultsOnAHTTP400FromDOIOrg(): void {
         $doiCrossrefStatus = new DOICrossrefStatus("10.1145/1998076.1998132", new DOISystemClientforTests(400), 'Crossref.org');
