56dcdea38c42b26b58c218cad756e616b694ecca
Jhon <jhon@lepidus.com.br>
2025-06-09 13:52:32 -0400

    Hides prefix and subtitle fields for authors in workflow
    Issue: documentacao-e-tarefas/scielo#777
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- ScieloScreeningPlugin.inc.php
+++ ScieloScreeningPlugin.inc.php
@@ -35,6 +35,7 @@ class ScieloScreeningPlugin extends GenericPlugin
             HookRegistry::register('Templates::Submission::SubmissionMetadataForm::AdditionalMetadata', array($this, 'metadataFieldEdit'));
             HookRegistry::register('Template::Workflow::Publication', array($this, 'addToPublicationForms'));
             HookRegistry::register('Template::Workflow::Publication', array($this, 'addGalleysWarning'));
+            HookRegistry::register('Form::config::after', array($this, 'hidePrefixAndSubtitleFieldsForAuthors'));
 
             HookRegistry::register('Schema::get::submission', [$this, 'addOurFieldsToSubmissionSchema']);
             HookRegistry::register('LoadComponentHandler', array($this, 'setupScieloScreeningHandler'));
@@ -281,6 +282,26 @@ class ScieloScreeningPlugin extends GenericPlugin
         $output = &$params[2];
 
         $output .= sprintf('%s', $smarty->fetch($this->getTemplateResource('addGalleysWarning.tpl')));
+    }
+
+    public function hidePrefixAndSubtitleFieldsForAuthors($hookName, $params)
+    {
+        $formConfig = &$params[0];
+        $form = $params[1];
+
+        if ($form->id !== 'titleAbstract' || !empty($form->errors)) {
+            return;
+        }
+
+        $submission = Services::get('submission')->get($form->publication->getData('submissionId'));
+        if (!$this->userIsAuthor($submission)) {
+            return;
+        }
+
+        $filteredFields = array_filter($formConfig['fields'], function ($field) {
+            return $field['name'] != 'prefix' && $field['name'] != 'subtitle';
+        });
+        $formConfig['fields'] = array_values($filteredFields);
     }
 
     public function listRules($hookName, $args)
