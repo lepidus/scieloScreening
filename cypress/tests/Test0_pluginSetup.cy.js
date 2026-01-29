8c96a7e62c37ba42d28a2017d6fcda30603c7d2b
yves <yves@lepidus.com.br>
2026-01-29 18:43:50 -0400

    Adapts Cypress Test: Plugin Setup.
    Issue: documentacao-e-tarefas/scielo#879
    
    Signed-off-by: yves <yves@lepidus.com.br>
    Signed-off-by: carlos <carlos@lepidus.com.br>

--- cypress/tests/Test0_pluginSetup.cy.js
+++ cypress/tests/Test0_pluginSetup.cy.js
@@ -8,30 +8,30 @@ describe('SciELO Screening - Plugin setup', function () {
 		cy.get('nav').contains('Website').click({ force: true });
 
 		cy.waitJQuery();
-		cy.get('#plugins-button').click();
+		cy.get('button[id="plugins-button"]').click();
 
 		cy.get('input[id^=select-cell-scieloscreeningplugin]').check();
 		cy.get('input[id^=select-cell-scieloscreeningplugin]').should('be.checked');
 	});
+
 	it('Configures plugin', function() {
 		cy.login('dbarnes', null, 'publicknowledge');
 		cy.get('nav').contains('Settings').click();
 		cy.get('nav').contains('Website').click({ force: true });
 
 		cy.waitJQuery();
-		cy.get('#plugins-button').click();
+		cy.get('button[id="plugins-button"]').click();
 
-		cy.contains('tr', 'SciELO Screening').within(() => {
-			cy.get('a.show_extras').click();
-			cy.contains('a', 'Settings').click();
-		});
+		cy.get('a[id^="component-grid-settings-plugins-settingsplugingrid-category-generic-row-scieloscreeningplugin-settings-button-"]', {timeout: 20_000}).as('settings');
+		cy.waitJQuery();
+		cy.get('@settings').click({force: true});
 
 		cy.get('#orcidAPIPath').select('Member Sandbox');
 		cy.get('input[name="orcidClientId"]').clear().type(Cypress.env('orcidClientId'), {delay: 0});
 		cy.get('input[name="orcidClientSecret"]').clear().type(Cypress.env('orcidClientSecret'), {delay: 0});
 
-		cy.get('#scieloScreeningSettingsForm').contains('button', 'Save').click();
-		cy.get('.pkpFormPage__status:contains("Saved")');
+		cy.get('form[id="scieloScreeningSettingsForm"] button[id^="submitFormButton"]').click();
+		cy.waitJQuery();
 		cy.contains('Please configure the ORCID API access for use in pulling ORCID records information').should('not.exist');
 	});
 });
