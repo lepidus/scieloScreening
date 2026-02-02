cb5f2074b64896f2d418ceff3a7b2b2bf27d1e7b
yves <yves@lepidus.com.br>
2026-02-02 18:42:51 -0400

    Adapts Submission Wizard tests to OPS 3.5 changes.
    Issue: documentacao-e-tarefas/scielo#879
    
    Signed-off-by: yves <yves@lepidus.com.br>
    Signed-off-by: carlos <carlos@lepidus.com.br>

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,35 +1,34 @@
 Cypress.Commands.add('findSubmission', function(tab, title) {
-	cy.get('#' + tab + '-button').click();
-    cy.get('.listPanel__itemSubtitle:visible:contains("' + title + '")').first()
-        .parent().parent().within(() => {
-            cy.get('.pkpButton:contains("View")').click();
-        });
+    cy.get('nav').contains(tab).click();
+    cy.contains('table tr', title).within(() => {
+        cy.get('button').contains(/Complete submission|View/).click({force: true});
+    });
 });
 
 Cypress.Commands.add('addSubmissionGalleys', (files) => {
-	files.forEach(file => {
-		cy.get('a[id^=component-grid-preprintgalleys-preprintgalleygrid-addGalley-button-]').contains("Add File").click();
-		cy.wait(2000); // Avoid occasional failure due to form init taking time
-		cy.get('div.pkp_modal_panel').then($modalDiv => {
-			cy.wait(3000);
-			$modalDiv.find('div.header:contains("Add File")');
-			cy.get('div.pkp_modal_panel input[id^="label-"]').type('PDF', {delay: 0});
-			cy.get('div.pkp_modal_panel button:contains("Save")').click();
-			cy.wait(2000); // Avoid occasional failure due to form init taking time
-		});
-		cy.get('select[id=genreId]').select(file.genre);
-		cy.fixture(file.file, 'base64').then(fileContent => {
-			cy.get('input[type=file]').attachFile(
-				{fileContent, 'filePath': file.fileName, 'mimeType': 'application/pdf', 'encoding': 'base64'}
-			);
-		});
-		cy.get('#continueButton').click();
-		cy.wait(2000);
-		for (const field in file.metadata) {
-			cy.get('input[id^="' + Cypress.$.escapeSelector(field) + '"]:visible,textarea[id^="' + Cypress.$.escapeSelector(field) + '"]').type(file.metadata[field], {delay: 0});
-			cy.get('input[id^="language"').click({force: true}); // Close multilingual and datepicker pop-overs
-		}
-		cy.get('#continueButton').click();
-		cy.get('#continueButton').click();
-	});
+    files.forEach(file => {
+        cy.get('a:contains("Add File")').click();
+        cy.wait(2000);
+        cy.get('[role="dialog"]').then(($modalDiv) => {
+            cy.wait(3000);
+            $modalDiv.find('div.header:contains("Add File")');
+            cy.get('[role="dialog"] input[id^="label-"]').type('PDF', {delay: 0});
+            cy.get('[role="dialog"] button:contains("Save")').click();
+            cy.wait(2000);
+        });
+        cy.get('select[id=genreId]').select(file.genre);
+        cy.fixture(file.file, 'base64').then(fileContent => {
+            cy.get('input[type=file]').attachFile(
+                {fileContent, 'filePath': file.fileName, 'mimeType': 'application/pdf', 'encoding': 'base64'}
+            );
+        });
+        cy.get('#continueButton').click();
+        cy.wait(2000);
+        for (const field in file.metadata) {
+            cy.get('input[id^="' + Cypress.$.escapeSelector(field) + '"]:visible,textarea[id^="' + Cypress.$.escapeSelector(field) + '"]').type(file.metadata[field], {delay: 0});
+            cy.get('input[id^="language"').click({force: true});
+        }
+        cy.get('#continueButton').click();
+        cy.get('#continueButton').click();
+    });
 });
