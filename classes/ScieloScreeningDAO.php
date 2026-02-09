b2d15fcae1c053901eb2872775f459d6f3fc83a6
Jhon <jhon@lepidus.com.br>
2026-02-09 14:36:36 -0400

    Filter SciELO Journal users from submissions
    Issue: documentacao-e-tarefas/scielo#884
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

---
+++ classes/ScieloScreeningDAO.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace APP\plugins\generic\scieloScreening\classes;
+
+use PKP\db\DAO;
+use Illuminate\Support\Facades\DB;
+
+class ScieloScreeningDAO extends DAO
+{
+    private const SCIELO_JOURNAL_ABBREV = 'SciELO';
+    private const SETTINGS_SEARCH_LOCALES = ['pt_BR', 'en'];
+
+    public function getScieloJournalUserGroupId(int $contextId): ?int
+    {
+        $result = DB::table('user_groups AS ug')
+            ->leftJoin('user_group_settings AS ugs', 'ug.user_group_id', '=', 'ugs.user_group_id')
+            ->where('ug.context_id', $contextId)
+            ->where('ugs.setting_name', 'abbrev')
+            ->where('ugs.setting_value', self::SCIELO_JOURNAL_ABBREV)
+            ->whereIn('ugs.locale', self::SETTINGS_SEARCH_LOCALES)
+            ->first();
+
+        if (is_null($result)) {
+            return null;
+        }
+
+        return get_object_vars($result)['user_group_id'];
+    }
+}
---
+++ classes/observers/listeners/FilterAuthorsOnSubmission.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace APP\plugins\generic\scieloScreening\classes\observers\listeners;
+
+use Illuminate\Events\Dispatcher;
+use PKP\observers\events\SubmissionSubmitted;
+use APP\facades\Repo;
+use APP\plugins\generic\scieloScreening\classes\ScieloScreeningDAO;
+
+class FilterAuthorsOnSubmission
+{
+    public function subscribe(Dispatcher $events): void
+    {
+        $events->listen(
+            SubmissionSubmitted::class,
+            FilterAuthorsOnSubmission::class
+        );
+    }
+
+    public function handle(SubmissionSubmitted $event): void
+    {
+        $submission = $event->submission;
+        $contextId = $event->context->getId();
+        $publication = $submission->getCurrentPublication();
+
+        $scieloScreeningDao = new ScieloScreeningDAO();
+        $scieloJournalRoleId = $scieloScreeningDao->getScieloJournalUserGroupId($contextId);
+
+        foreach ($publication->getData('authors') as $author) {
+            $user = Repo::user()->getByEmail($author->getData('email'));
+            if (is_null($user)) {
+                continue;
+            }
+
+            if ($user->hasRole($scieloJournalRoleId, $contextId)) {
+                Repo::author()->delete($author);
+            }
+        }
+    }
+}
